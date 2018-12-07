409e93561eb52a485b8de2b167d09880c79f025e
Harald Klein <hari@vt100.at>
2018-12-07 13:55:53 +0100

    add defines for values, use UART stepper control instead of timer interrupt for motor A

--- powderomatic-step.ino
+++ powderomatic-step.ino
@@ -1,22 +1,22 @@
-/*  
- *   
- *   Powder'O'Matic Step
- *
- *  Copyright (c) 2018 Harald Klein <hari@vt100.at>
- *
- *  Licensed under GPL Version 3 - https://www.gnu.org/licenses/gpl-3.0.html
- *
- *  This Sketch controls two stepper motors via two TCM2208 stepper drivers. The drivers 
- *  are controlled via dir/step pins and configured via Serial 2 and 3. Serial 1 is being
- *  used to fetch values from a Kern PCB scale. The target value is adjusted with a KY-040
- *  rotary encoder. Dispensing can be started by pressing the push button of the rotary
- *  encoder. A SSD1306 OLED display is used to display the current state, actual and target
- *  value. The Kern scale needs to be configured for command mode ("rE Cr") with 19200 baud. 
- *  The default values are set up for grain.
- *  
- *  The KY-040 is evaluated by attaching a pin interrupt to the CLK signal (rotaryIsr())
- *  The Stepper A is being turned via a timer interrupt (timer 5). Speed is adjusted by the
- *  microsteps() function of the TMC2208Stepper library.
+/*
+
+     Powder'O'Matic Step
+
+    Copyright (c) 2018 Harald Klein <hari@vt100.at>
+
+    Licensed under GPL Version 3 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    This Sketch controls two stepper motors via two TCM2208 stepper drivers. The drivers
+    are controlled via dir/step pins and configured via Serial 2 and 3. Serial 1 is being
+    used to fetch values from a Kern PCB scale. The target value is adjusted with a KY-040
+    rotary encoder. Dispensing can be started by pressing the push button of the rotary
+    encoder. A SSD1306 OLED display is used to display the current state, actual and target
+    value. The Kern scale needs to be configured for command mode ("rE Cr") with 19200 baud.
+    The default values are set up for grain.
+
+    The KY-040 is evaluated by attaching a pin interrupt to the CLK signal (rotaryIsr())
+    The Stepper A is being turned via a timer interrupt (timer 5). Speed is adjusted by the
+    microsteps() function of the TMC2208Stepper library.
 */
 
 /*
@@ -27,50 +27,74 @@
 */
 
 // grain values
-#define MASSFILL_FAST_DELTA 1.2
+#define MASSFILL_FAST_DELTA 1.4  // 1.2
 #define MASSFILL_SLOW_DELTA 0.3
-#define TRICKLE_DELTA 0.07
+#define TRICKLE_DELTA 0.06
+#define TRICKLE_DELAY 200
+
+#define MOTA_SPEED_MAX 210000
+#define MOTA_SPEED_MIN 15000
 
 #define debug 20
 
-// trickler
+// trickler stepper setup
 #define MOTA_DIR 8
 #define MOTA_STEP 11
 #define MOTA_ENA 13
-#define MOTA_MSTEPS_FAST 16
-#define MOTA_MSTEPS_SLOW 64
-#define MOTA_MSTEPS_TRICKLE 256
+#define MOTA_MSTEPS 256 // 16
+//#define MOTA_MSTEPS_SLOW 64
+//#define MOTA_MSTEPS_TRICKLE 256
+#define MOTA_SERIAL Serial2
+#define MOTA_CURRENT_MA 300
 
-// thrower
+// thrower stepper setup
 #define MOTB_DIR 5
 #define MOTB_STEP 3
 #define MOTB_ENA 6
 #define MOTB_MSTEPS 32
+#define MOTB_SERIAL Serial3
+#define MOTB_CURRENT_MA 300
 
+// baudrates
+#define TMC2208_BAUDRATE 460800
+#define KERN_PCB_BAUDRATE 19200
+#define CONSOLE_BAUDRATE 115200
+
+// display settings
+#define OLED_RESET -1
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define DISPLAY_I2C_ADDRESS 0x3C
+
+// rotary encoder settings
 #define K040_SW 22  // D22, pin 74
 #define K040_DT 24 // D24, pin 72
 #define K040_CLK 2   //  int pin  alt: // D26, pin 70
 
-#include <TMC2208Stepper.h>                       
-TMC2208Stepper driverA = TMC2208Stepper(&Serial2);
-TMC2208Stepper driverB = TMC2208Stepper(&Serial3);
+#define ROTARY_DELTA_VALUE 0.1
+#define ROTARY_DEBOUNCE_MILLIS 5
+
+// config section end
+
+
+
+#include <TMC2208Stepper.h>
+TMC2208Stepper driverA = TMC2208Stepper(&MOTA_SERIAL);
+TMC2208Stepper driverB = TMC2208Stepper(&MOTB_SERIAL);
 
 #include <AccelStepper.h>
-AccelStepper stepperA(AccelStepper::DRIVER, MOTA_STEP, MOTA_DIR);
+// AccelStepper stepperA(AccelStepper::DRIVER, MOTA_STEP, MOTA_DIR);
 AccelStepper stepperB(AccelStepper::DRIVER, MOTB_STEP, MOTB_DIR);
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define OLED_RESET -1
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 volatile bool turn = false;
-volatile float targetValue = 4;
+volatile float targetValue = 10;
 volatile int virtualPosition = 0;
 
 void rotaryISR() {
@@ -78,26 +102,26 @@ void rotaryISR() {
   unsigned long interruptTime = millis();
 
   // debounce 5ms
-  if (interruptTime - lastInterruptTime > 5) {
+  if (interruptTime - lastInterruptTime > ROTARY_DEBOUNCE_MILLIS) {
     if (digitalRead(K040_DT) == LOW)
     {
-      targetValue = targetValue - 0.1;
+      targetValue = targetValue - ROTARY_DELTA_VALUE;
     }
     else {
-      targetValue = targetValue + 0.1;
+      targetValue = targetValue + ROTARY_DELTA_VALUE;
     }
     lastInterruptTime = interruptTime;
   }
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial1.begin(19200);
-  Serial2.begin(115200);
-  Serial3.begin(115200);
+  Serial.begin(CONSOLE_BAUDRATE);
+  Serial1.begin(KERN_PCB_BAUDRATE);
+  Serial2.begin(TMC2208_BAUDRATE);
+  Serial3.begin(TMC2208_BAUDRATE);
 
   Serial.println("Powder'O'Matic Step startup...");
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  if (!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_I2C_ADDRESS)) { // Address 0x3C
     Serial.println("SSD1306 allocation failed");
   }
   display.clearDisplay();
@@ -136,17 +160,17 @@ void setup() {
 
   driverA.pdn_disable(true);     // Use PDN/UART pin for communication
   driverA.I_scale_analog(true); // Use internal voltage reference
-  driverA.rms_current(300);      // Set driver current 500mA
+  driverA.rms_current(MOTA_CURRENT_MA);      // Set driver current 500mA
   // driverB.en_spreadCycle(1);
   driverA.toff(2);               // Enable driver in software
   driverA.mstep_reg_select(true); // ignore MS1 + MS2
-  driverA.microsteps(MOTA_MSTEPS_FAST);
+  driverA.microsteps(MOTA_MSTEPS);
   driverA.ihold(0);
   digitalWrite(MOTA_DIR, LOW);
 
   driverB.pdn_disable(true);     // Use PDN/UART pin for communication
   driverB.I_scale_analog(true); // Use internal voltage reference
-  driverB.rms_current(300);      // Set driver current 500mA
+  driverB.rms_current(MOTB_CURRENT_MA);      // Set driver current 500mA
   // driverB.en_spreadCycle(1);
   driverB.toff(2);               // Enable driver in software
   driverB.mstep_reg_select(true); // ignore MS1 + MS2
@@ -168,18 +192,17 @@ void setup() {
     Serial.println(data, HEX);
   }
 
-  stepperA.setMaxSpeed(20000000);
-  stepperA.setAcceleration(1000);
+  //stepperA.setMaxSpeed(20000000);
+  //stepperA.setAcceleration(1000);
   //  stepperA.moveTo(5000);
   //   stepperA.runToPosition();
 
   stepperB.setMaxSpeed(20000000);
-  stepperB.setAcceleration(200000);
-  stepperB.moveTo(200 * MOTB_MSTEPS);
+  stepperB.setAcceleration(2000);
+  //stepperB.moveTo(200 * MOTB_MSTEPS);
   // stepperB.runToPosition();
 
   //setup Timer5 for stepper A steps
-  TCCR5A = 0b00000000; // no pwm
   /*
       CS12  CS11  CS10
          0     0     0    - Stop timer/counter
@@ -188,22 +211,43 @@ void setup() {
          0     1     1    - by 64
          1     0     0    - by 256
          1     0     1    - by 1024
-         1     1     0    - inc falling
+          1     1     0    - inc falling
          1     1     1    - inc rising
   */
-  TCCR5B = 0b00001101;     // clear timer counter on compare, CS12-CS10 == 1024 divisor
-  TIMSK5 |= 0b00000010;    // set for output compare interrupt
-  sei();                   // enable interrups
 
-  turn = false;
+  /*
+    TCCR5B = 0b00001101;     // clear timer counter on compare, CS12-CS10 == 1024 divisor
+    TCCR5A = 0b00000000; // no pwm
 
+    TIMSK5 |= 0b00000010;    // set for output compare interrupt
+    sei();                   // enable interrups
+  */
 }
-
-// timer interrupt to toggle direction output for stepper A
-ISR(TIMER5_COMPA_vect) {
+/*
+  // timer interrupt to toggle direction output for stepper A
+  ISR(TIMER5_COMPA_vect) {
   if (turn) {
     digitalWrite(MOTA_STEP,  !digitalRead(MOTA_STEP));
   }
+  }
+*/
+
+
+// derive motor speed from value
+uint32_t speedFromValue(float currentValue, float targetValue) {
+  float delta = targetValue - currentValue;
+  if (delta > MASSFILL_FAST_DELTA) {
+    Serial.println("MAX Speed");
+    return MOTA_SPEED_MAX;
+  }
+  if (delta < MASSFILL_SLOW_DELTA) {
+    Serial.println("MIN Speed");
+    return MOTA_SPEED_MIN;
+  }
+  uint32_t newSpeed = map(delta * 10, MASSFILL_SLOW_DELTA * 10, MASSFILL_FAST_DELTA * 10, MOTA_SPEED_MIN, MOTA_SPEED_MAX);
+  Serial.print("Speed: ");
+  Serial.println(newSpeed);
+  return newSpeed;
 }
 
 // method to read a value from the serially attached KERN scale
@@ -335,6 +379,7 @@ void updateDisplay() {
   display.display();
   // Serial.println("update display end");
 }
+unsigned long startTimestamp;
 
 void loop() {
   switch (currentState) {
@@ -343,6 +388,7 @@ void loop() {
 
       if (digitalRead(K040_SW) == LOW) {
         currentState = WAIT_FOR_TARE;
+        updateDisplay();
         taraScale();
       }
 
@@ -351,32 +397,31 @@ void loop() {
       Serial.println("STATE:WAIT_FOR_TARE");
       while (readScaleStable(&value) != 1);
       if (value == 0.0) {
+        startTimestamp = millis();
         currentState = MASSFILL_FAST;
-        driverA.microsteps(MOTA_MSTEPS_FAST);
-        turn = true;
+        driverA.VACTUAL(speedFromValue(value, targetValue));
       }
       break;
     case MASSFILL_FAST:
       Serial.println("STATE:MASSFILL_FAST");
       if (readScaleStableOrUnstable(&value) != -1) {
-
-        Serial.print("Value: ");
-        Serial.println(value);
-
+        /*
+                Serial.print("Value: ");
+                Serial.println(value);
+        */
         if (value > targetValue - MASSFILL_FAST_DELTA) {
           currentState = MASSFILL_SLOW;
-          driverA.microsteps(MOTA_MSTEPS_SLOW);
+          driverA.VACTUAL(speedFromValue(value, targetValue));
         }
       }
       break;
     case MASSFILL_SLOW:
       Serial.println("STATE:MASSFILL_SLOW");
       if (readScaleStableOrUnstable(&value) != -1) {
-
+        driverA.VACTUAL(speedFromValue(value, targetValue));
         if (value > targetValue - MASSFILL_SLOW_DELTA) {
           currentState = TRICKLE;
-          turn = false;
-          driverA.microsteps(MOTA_MSTEPS_TRICKLE);
+          driverA.VACTUAL((uint32_t) 0);
         }
       }
       break;
@@ -388,16 +433,19 @@ void loop() {
           currentState = FINISHED;
         } else {
           Serial.println("trickling..");
-          turn = true;
+          driverA.VACTUAL(speedFromValue(value, targetValue));
         }
       } else {
         Serial.println("no stable");
-        turn = false;
-        delay(100);
+        driverA.VACTUAL((uint32_t) 0);
+        delay(TRICKLE_DELAY);
       }
       break;
     case FINISHED:
       Serial.println("STATE:FINISHED");
+      Serial.print("Total time: ");
+      Serial.println((millis() - startTimestamp) / 1000);
+      delay(5000);
       if (digitalRead(K040_SW) == LOW) {
         currentState = IDLE;
       } else if (readScaleStableOrUnstable(&value) == 1) {
