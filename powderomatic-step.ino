6db3db7522b81a14e9c5ee8a893d488ac5b49257
Harald Klein <hari@vt100.at>
2018-12-21 16:04:09 +0100

    add A&D FX120i support, change pin definitions to match the v1 PCB, use internal pullups for the rotary pins

--- powderomatic-step.ino
+++ powderomatic-step.ino
@@ -26,33 +26,47 @@
   #define TRICKLE_DELTA 0.01
 */
 
+// #define BALANCE_TYPE 1 // kern PCB 100-3
+// #define BALANCE_TYPE 2 // A&D FX120i
+#define BALANCE_TYPE 2
+
+// settings for Kern PCB
+#if BALANCE_TYPE == 1
 // grain values
 #define MASSFILL_FAST_DELTA 1.4  // 1.2
 #define MASSFILL_SLOW_DELTA 0.3
 #define TRICKLE_DELTA 0.06
 #define TRICKLE_DELAY 200
 
+// settings for A&D FX120i
+#elif BALANCE_TYPE == 2
+#define MASSFILL_FAST_DELTA 0.8  // 1.2
+#define MASSFILL_SLOW_DELTA 0.1
+#define TRICKLE_DELTA 0.04
+#define TRICKLE_DELAY 0
+
+#endif
+
+
 #define MOTA_SPEED_MAX 210000
 #define MOTA_SPEED_MIN 15000
 
-#define AUTOMODE true
+#define AUTOMODE false
 
 #define debug 20
 
 // trickler stepper setup
-#define MOTA_DIR 8
-#define MOTA_STEP 11
-#define MOTA_ENA 13
+#define MOTA_DIR 24
+#define MOTA_STEP 23
+#define MOTA_ENA 22
 #define MOTA_MSTEPS 256 // 16
-//#define MOTA_MSTEPS_SLOW 64
-//#define MOTA_MSTEPS_TRICKLE 256
 #define MOTA_SERIAL Serial2
 #define MOTA_CURRENT_MA 300
 
 // thrower stepper setup
-#define MOTB_DIR 5
-#define MOTB_STEP 3
-#define MOTB_ENA 6
+#define MOTB_DIR 27
+#define MOTB_STEP 26
+#define MOTB_ENA 25
 #define MOTB_MSTEPS 2
 #define MOTB_SERIAL Serial3
 #define MOTB_CURRENT_MA 300
@@ -61,7 +75,7 @@
 #define THROWER_DELAY 300
 #define THROWER_ENABLED true
 
-#define BUZZER_PIN -1
+#define BUZZER_PIN 9
 #define BUZZER_SUCCESS_FREQ 2000
 #define BUZZER_SUCCESS_DURATION 500
 #define BUZZER_ERROR_FREQ 4000
@@ -72,6 +86,7 @@
 // baudrates
 #define TMC2208_BAUDRATE 460800
 #define KERN_PCB_BAUDRATE 19200
+
 #define CONSOLE_BAUDRATE 115200
 
 // display settings
@@ -81,8 +96,8 @@
 #define DISPLAY_I2C_ADDRESS 0x3C
 
 // rotary encoder settings
-#define K040_SW 22  // D22, pin 74
-#define K040_DT 24 // D24, pin 72
+#define K040_SW 4  // D22, pin 74
+#define K040_DT 3 // D24, pin 72
 #define K040_CLK 2   //  int pin  alt: // D26, pin 70
 
 #define ROTARY_DELTA_VALUE 0.1
@@ -107,15 +122,14 @@ AccelStepper stepperB(AccelStepper::DRIVER, MOTB_STEP, MOTB_DIR);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-volatile bool turn = false;
-volatile float targetValue = 10;
+volatile float targetValue = 5;
 bool automaticModeEnabled = AUTOMODE;
 
 void rotaryISR() {
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
   unsigned long delta = millis() - lastInterruptTime;
-  int multi = sq(500-min(delta,500))/25000 +1;
+  int multi = sq(500 - min(delta, 500)) / 25000 + 1;
 
   // debounce 5ms
   if (delta > ROTARY_DEBOUNCE_MILLIS) {
@@ -154,9 +168,9 @@ void setup() {
 
   display.display();
 
-  pinMode(K040_DT, INPUT);
+  pinMode(K040_DT, INPUT_PULLUP);
   pinMode(K040_SW, INPUT_PULLUP);
-  pinMode(K040_CLK, INPUT);
+  pinMode(K040_CLK, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(K040_CLK), rotaryISR, LOW);
 
@@ -172,6 +186,9 @@ void setup() {
 
   pinMode(MOTB_STEP, OUTPUT);
   pinMode(MOTB_DIR, OUTPUT);
+
+  if (BUZZER_PIN)
+    pinMode(BUZZER_PIN, OUTPUT);
 
   digitalWrite(MOTA_ENA, HIGH);   // Disable driver in hardware
   digitalWrite(MOTB_ENA, HIGH);
@@ -243,46 +260,92 @@ uint32_t speedFromValue(float currentValue, float targetValue) {
 // method to read a value from the serially attached KERN scale
 int readScale(float *returnValue) {
   if (debug > 39) Serial.println("read scale");
-  Serial1.write("w");
-  Serial1.flush();
-  delay(300);
-  if (Serial1.available()) {
-    // while (!(Serial1.available() > 0)) {};
-    // if (1) {
-    bool negative = false;
-    bool stable = false;
-    String scaleOutput = Serial1.readStringUntil('\n');
-    if (debug > 19) Serial.println(scaleOutput);
+  if (BALANCE_TYPE ==  1) {
+    Serial1.write("w");
+    Serial1.flush();
+    delay(300);
+    if (Serial1.available()) {
+      // while (!(Serial1.available() > 0)) {};
+      // if (1) {
+      bool negative = false;
+      bool stable = false;
+      String scaleOutput = Serial1.readStringUntil('\n');
+      if (debug > 19) Serial.println(scaleOutput);
 
-    if ((scaleOutput.indexOf('.') == 8) || scaleOutput.indexOf('.') == 9) { // comma separator at pos 9 indicates good string [ for gn, 8 for g]
-      if (scaleOutput.indexOf('-') != -1) {
-        negative = true;
+      if ((scaleOutput.indexOf('.') == 8) || scaleOutput.indexOf('.') == 9) { // comma separator at pos 9 indicates good string [ for gn, 8 for g]
+        if (scaleOutput.indexOf('-') != -1) {
+          negative = true;
+        }
+        if (scaleOutput.indexOf("g") != -1) {
+          stable = true;
+        }
+        String valueString = scaleOutput.substring(2, 12);
+        valueString.trim();
+        //Serial.print("Valuestring: ");
+        //Serial.println(valueString);
+        float value = valueString.toFloat();
+        if (negative) value = value * -1;
+        if (debug > 29) Serial.print("Float: ");
+        if (debug > 29) Serial.println(value, 3);
+        *returnValue = value;
+        if (stable) return 1;
+        return 0;
+      } else {
+        return -1;
       }
-      if (scaleOutput.indexOf("g") != -1) {
-        stable = true;
-      }
-      String valueString = scaleOutput.substring(2, 12);
-      valueString.trim();
-      //Serial.print("Valuestring: ");
-      //Serial.println(valueString);
-      float value = valueString.toFloat();
-      if (negative) value = value * -1;
-      if (debug > 29) Serial.print("Float: ");
-      if (debug > 29) Serial.println(value, 3);
-      *returnValue = value;
-      if (stable) return 1;
-      return 0;
     } else {
+      if (debug > 39) Serial.println("no data from scale");
       return -1;
     }
-  } else {
-    if (debug > 39) Serial.println("no data from scale");
-    return -1;
+  } else { // A&D FX120i
+    Serial1.write("Q\r\n");
+    delay(50);
+    if (Serial1.available()) {
+      //Serial.println("data available");
+      bool negative = false;
+      bool stable = false;
+      String scaleOutput = Serial1.readStringUntil('\n');
+      if (debug > 29) {
+        Serial.print("SCALE OUTPUT:");
+        Serial.println(scaleOutput);
+        Serial.println(scaleOutput.indexOf("."));
+      }
+      if ((scaleOutput.indexOf(".") == 8) || (scaleOutput.indexOf('.') == 9)) {
+        if (scaleOutput.indexOf('ST') != -1) {
+          //Serial.println("stable");
+          stable = true;
+        }
+        if (scaleOutput.indexOf('-') != -1) {
+          negative = true;
+
+        }
+        String valueString = scaleOutput.substring(4, 12);
+        if (debug > 39) {
+          Serial.println(valueString);
+
+        }
+        float value = valueString.toFloat();
+        if (negative) value = value * -1;
+        if (debug > 29) Serial.print("Float: ");
+        if (debug > 29) Serial.println(value, 3);
+        *returnValue = value;
+        if (stable) return 1;
+        return 0;
+      } else {
+        return -1;
+      }
+
+    } else {
+      if (debug > 39) Serial.println("no data from scale");
+      return -1;
+    }
   }
+  // wrong scale configuration
+  return -1;
 }
 
 // helper to loop until a value has been read from the scale
-int readScaleStableOrUnstable(float *returnValue) {
+int readScaleStableOrUnstable(float * returnValue) {
   int stable = 0;
   int errcount = 0;
   float value;
@@ -290,7 +353,6 @@ int readScaleStableOrUnstable(float *returnValue) {
   while (stable == -1) {
     stable = readScale(&value);
     if (errcount++ > 10) {
-      turn = false;
       return -1;
     }
   }
@@ -299,14 +361,13 @@ int readScaleStableOrUnstable(float *returnValue) {
 }
 
 // helper to loop until a stable value has been read
-int readScaleStable(float *returnValue) {
+int readScaleStable(float * returnValue) {
   int stable = 0;
   int errcount = 0;
   float value;
   while (stable != 1) {
     stable = readScale(&value);
     if (errcount++ > 10) {
-      turn = false;
       return -1;
     }
   }
@@ -316,11 +377,20 @@ int readScaleStable(float *returnValue) {
 
 // tara the scale
 void taraScale() {
-  while (Serial1.available()) Serial1.read();
-  Serial.println("Tara scale");
-  Serial1.write("t");
-  Serial1.flush();
-  delay(2500);
+  if (BALANCE_TYPE == 1) { // KERN PCB
+    while (Serial1.available()) Serial1.read();
+    Serial.println("Tara scale");
+    Serial1.write("t");
+    Serial1.flush();
+    delay(2500);
+  } else {  // A&D FX120i
+    // T - Tare, Z - ReZero,
+    Serial1.write("Z\r\n");
+    Serial1.flush();
+    // delay(500);
+
+  }
+
 }
 
 enum state {
@@ -333,7 +403,21 @@ enum state {
   ERROR
 };
 
+String stateToString(int myState) {
+  switch(myState) {
+    case 0: return "BEREIT";
+    case 1: return "TARIERE";
+    case 2: return "SCHNELL";
+    case 3: return "LANGSAM";
+    case 4: return "TRICKLE";
+    case 5: return "FERTIG";
+    case 6: return "FEHLER";
+    default: return "STATUS?";
+  }  
+}
+
 state currentState = IDLE;
+state lastState = IDLE;
 float value = -1.0;
 
 void updateDisplay() {
@@ -373,9 +457,39 @@ void updateDisplay() {
 unsigned long startTimestamp;
 
 void loop() {
+  // #define TESTMODE 1
+
+#ifdef TESTMODE
+  driverA.VACTUAL(speedFromValue(5, 50));
+  while (true) {
+    Serial.println("throw");
+    throwPowder();
+    Serial.println("sleep");
+    tone(BUZZER_PIN, BUZZER_SUCCESS_FREQ, BUZZER_SUCCESS_DURATION);
+
+    delay(3000);
+  }
+#endif
+
+  if (currentState != lastState) {
+    Serial.print("State change: ");
+/*    String status;
+    switch (currentState) {
+      case 0: status = " BEREIT  "; break;
+      case 1: status = " TARIERE "; break;
+      case 2: status = " SCHNELL "; break;
+      case 3: status = " LANGSAM "; break;
+      case 4: status = " TRICKLE "; break;
+      case 5: status = " FERTIG  "; break;
+      case 6: status = " FEHLER  "; break;
+      default: status = " STATUS? ";
+    } */
+    Serial.println(stateToString(currentState));
+    lastState = currentState;
+  }
   switch (currentState) {
     case IDLE:
-      Serial.println("STATE:IDLE");
+      //Serial.println("STATE:IDLE");
       readScaleStableOrUnstable(&value);
 
       if ((digitalRead(K040_SW) == LOW) || (automaticModeEnabled && (value < 0.1 && value > -0.1)) ) {
@@ -386,7 +500,7 @@ void loop() {
 
       break;
     case WAIT_FOR_TARE:
-      Serial.println("STATE:WAIT_FOR_TARE");
+      //Serial.println("STATE:WAIT_FOR_TARE");
       while (readScaleStable(&value) != 1)
         updateDisplay();
       if (value == 0.0) {
@@ -398,7 +512,7 @@ void loop() {
       }
       break;
     case MASSFILL_FAST:
-      Serial.println("STATE:MASSFILL_FAST");
+      //Serial.println("STATE:MASSFILL_FAST");
       if (readScaleStableOrUnstable(&value) != -1) {
         /*
                 Serial.print("Value: ");
@@ -411,33 +525,45 @@ void loop() {
       }
       break;
     case MASSFILL_SLOW:
-      Serial.println("STATE:MASSFILL_SLOW");
+      //Serial.println("STATE:MASSFILL_SLOW");
       if (readScaleStableOrUnstable(&value) != -1) {
         driverA.VACTUAL(speedFromValue(value, targetValue));
         if (value > targetValue - MASSFILL_SLOW_DELTA) {
           currentState = TRICKLE;
-          driverA.VACTUAL((uint32_t) 0);
+          if (BALANCE_TYPE == 1) // give the kern pcb some time to get stable
+            driverA.VACTUAL((uint32_t) 0);
         }
       }
       break;
     case TRICKLE:
-      Serial.println("STATE:TRICKLE");
-      if (readScaleStableOrUnstable(&value) == 1) {
-        if (value > targetValue - TRICKLE_DELTA) {
-          turn = false;
-          currentState = FINISHED;
+      //Serial.println("STATE:TRICKLE");
+      if (BALANCE_TYPE == 1) {
+        if (readScaleStableOrUnstable(&value) == 1) {
+          if (value > targetValue - TRICKLE_DELTA) {
+            currentState = FINISHED;
+            driverA.VACTUAL((uint32_t) 0);
+          } else {
+            Serial.println("trickling..");
+            driverA.VACTUAL(speedFromValue(value, targetValue));
+          }
         } else {
-          Serial.println("trickling..");
-          driverA.VACTUAL(speedFromValue(value, targetValue));
+          Serial.println("no stable");
+          driverA.VACTUAL((uint32_t) 0);
+          delay(TRICKLE_DELAY);
         }
       } else {
-        Serial.println("no stable");
-        driverA.VACTUAL((uint32_t) 0);
-        delay(TRICKLE_DELAY);
+        // A&D FX120i
+        readScaleStableOrUnstable(&value);
+        if (value > targetValue - TRICKLE_DELTA) {
+          currentState = FINISHED;
+          driverA.VACTUAL((uint32_t) 0);
+        }
+
       }
       break;
     case FINISHED:
-      Serial.println("STATE:FINISHED");
+      //Serial.println("STATE:FINISHED");
+      driverA.VACTUAL((uint32_t) 0);
       Serial.print("Total time: ");
       Serial.println((millis() - startTimestamp) / 1000);
       delay(500);;
@@ -455,7 +581,8 @@ void loop() {
       }
       break;
     case ERROR:
-      Serial.println("STATE:ERROR");
+      //Serial.println("STATE:ERROR");
+      driverA.VACTUAL((uint32_t) 0);
       for (int i = 0; i < BUZZER_ERROR_BEEP_COUNT; i++) {
 #ifdef BUZZER_PIN
         tone(BUZZER_PIN, BUZZER_ERROR_FREQ, BUZZER_ERROR_DURATION);
